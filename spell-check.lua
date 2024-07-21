cd5334dfc006d39ea6874256d64ddddec0f26314
uchitsa <uchitsa@ya.ru>
2024-07-22 02:05:04 +0300

    Update spell-check.lua

--- spell-check.lua
+++ spell-check.lua
@@ -1,5 +1,13 @@
 local spell_check = function(filename)
-  local cmd = string.format("aspell -t -c %s",filename)
+  local cmd = string.format("aspell -t -c %s", filename)
+  local handle = io.popen(cmd)
+  local result = handle:read("*a")
+  handle:close()
+
+  if result:match("Error:") then
+    print("spell check failed")
+    os.exit(1)
+  end
 end
 
 local main = function()
@@ -9,6 +17,7 @@ local main = function()
   else
     print("latex file is not defined")
     os.exit(1)
+  end
 end
 
 main()

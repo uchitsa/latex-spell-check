d3b15561ac6eec7acfeecd6026187b2d5d6d2793
uchitsa <uchitsa@ya.ru>
2024-07-22 02:00:51 +0300

    Update spell-check.lua

--- spell-check.lua
+++ spell-check.lua
@@ -3,7 +3,12 @@ local spell_check = function(filename)
 end
 
 local main = function()
-
+  local filename = os.getenv("TEXTINPUTS")
+  if filename then
+    spell_check(filename)
+  else
+    print("latex file is not defined")
+    os.exit(1)
 end
 
 main()
